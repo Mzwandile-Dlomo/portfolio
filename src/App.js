e1fb74d24f7d3890150b82f8d786663299402d66
Mzwandile Dlomo <mdlomo023a@student.wethinkcode.co.za>
2024-05-19 12:24:54 +0200

    navbar added, no styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import  Navbar  from './components/Navbar';
+import styled from 'styled-components';
+import { useRef, useEffect, useState } from 'react';
+
+
+
+const AppContainer = styled.div`
+  min-height: 100vh;         /* Equivalent to min-h-screen */
+  background-color: #132a13; /* Equivalent to bg-[#132a13] */
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const MainContainer = styled.div`
+  background-color: #fff;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1280px;
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+  padding-top: 0;
+  padding-bottom: 0;
+  font-family: sans-serif;
+
+  @media (min-width: 768px) {
+    padding-left: 3rem;
+    padding-right: 3rem;
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+
+  @media (min-width: 1024px) {
+    padding-left: 3rem;
+    padding-right: 3rem;
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+`;
+
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true); // Set initial loading state
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Navbar/>
+      <MainContainer>
+        <h1>Hello, world!</h1>
+      </MainContainer>
+    </AppContainer>
   );
 }
 
