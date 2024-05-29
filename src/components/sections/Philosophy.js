fe64d44db79ccf7adf0b4e5525df678a41f9611c
Mzwandile Dlomo <mdlomo023a@student.wethinkcode.co.za>
2024-05-29 16:30:44 +0200

    section2 save

--- src/components/sections/Philosophy.js
+++ src/components/sections/Philosophy.js
@@ -12,50 +12,60 @@ const Container = styled.div`
 
 
 const Title = styled.h1`
-//   font-family: 'Nunito', 'Montserrat', sans-serif;
-  font-size: 1.9rem;
+  font-family: 'Nunito', 'Montserrat', sans-serif;
+  font-size: 1.2rem;
   text-align: center;
   font-weight: 600;
-//   line-height: 1.2;
   color: #9BCF53;
-//   margin: 20px 0;
-//   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
-//   perspective: 1000px;
+  margin: 20px 0;
   cursor: pointer;
 
 `;
 
-const Subtitle = styled.h2`
+const Subtitle = styled.h3`
   margin-top: 0.75rem;
   text-align: center;
-  font-size: 1.125rem;
+  font-size: 0.4rem;
   font-weight: medium;
   color: #69b25b;
 
   @media (min-width: 640px) {
-    font-size: 1.25rem;
+    font-size: 1.05rem;
   }
 `;
 
 const Emphasis = styled.span`
   font-family: 'Caveat', cursive;
-  font-size: 24px;
   font-weight: bold;
   color: white;
   padding-right: 5px;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+  perspective: 1000px;
 
   &:hover{
     color: #9BCF53;
-    // background-color: red;
   }
 `;
 
+
+const First = styled.span`
+  font-family: 'Caveat', 'sans';
+  font-weight: bold;
+  color: green;
+  padding-right: 5px;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+  perspective: 1000px;
+
+  &:hover{
+    color: #9BCF53;
+  }
+`;
 
 const Philosophy = () => {
   return (
     <Container>
         <Title><Emphasis>My Life Philosophy</Emphasis></Title>
-        <Subtitle>Dedicated to creating a more open, secure, and connected world through innovative software solutions.</Subtitle>
+        <Subtitle><First className=''>Dedicated</First> to creating a more open, secure, and connected world through innovative software solutions.</Subtitle>
     </Container>
   )
 }
